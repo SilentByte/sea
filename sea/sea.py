cf4f819dd995e5a9ae320181a2f9136e236679c1
Rico A. Beti <rico.beti@silentbyte.com>
2024-04-30 22:47:32 +0800

    Implement document ingest

--- sea/sea.py
+++ sea/sea.py
@@ -5,38 +5,41 @@
 
 
 from typing import Any
+from textwrap import dedent
+
+import pyspark.sql.functions as F
 
 from sea.config import SeaConfig
 
-CONFIG = SeaConfig()
-
 
 class SeaRuntime:
-    def __init__(self, spark):
+    def __init__(self, config: SeaConfig, spark):
+        self.config = config
         self.spark = spark
 
     def spark_query(self, query: str, args: dict[str, Any] | list | None = None, **kwargs: Any) -> Any:
+        query = dedent(query).strip()
         print(query)
         return self.spark.sql(query, args, **kwargs)
 
-    def initialize_spark(self):
+    def initialize_spark(self) -> None:
         current_catalog = self.spark_query('SELECT current_catalog() AS name').collect()[0]['name']
         available_catalogs = [
             row['catalog']
             for row in self.spark_query('SHOW CATALOGS').collect()
         ]
 
-        if current_catalog != CONFIG.catalog:
+        if current_catalog != self.config.catalog:
             if current_catalog not in available_catalogs:
-                self.spark_query(f'CREATE CATALOG IF NOT EXISTS `{CONFIG.catalog}`')
-                self.spark_query(f'ALTER CATALOG `{CONFIG.catalog}` OWNER TO `account users`')
+                self.spark_query(f'CREATE CATALOG IF NOT EXISTS `{self.config.catalog}`')
+                self.spark_query(f'ALTER CATALOG `{self.config.catalog}` OWNER TO `account users`')
 
-            self.spark_query(f'CREATE SCHEMA IF NOT EXISTS `{CONFIG.catalog}`.`{CONFIG.schema}`')
-            self.spark_query(f'GRANT CREATE, USAGE ON DATABASE `{CONFIG.catalog}`.`{CONFIG.schema}` TO `account users`')
+            self.spark_query(f'CREATE SCHEMA IF NOT EXISTS `{self.config.catalog}`.`{self.config.schema}`')
+            self.spark_query(f'GRANT CREATE, USAGE ON DATABASE `{self.config.catalog}`.`{self.config.schema}` TO `account users`')
 
-        self.spark_query(f'USE `{CONFIG.catalog}`.`{CONFIG.schema}`')
+        self.spark_query(f'USE `{self.config.catalog}`.`{self.config.schema}`')
 
-        self.spark_query(f'CREATE VOLUME IF NOT EXISTS `{CONFIG.catalog}`.`{CONFIG.schema}`.`{CONFIG.volume}`')
+        self.spark_query(f'CREATE VOLUME IF NOT EXISTS `{self.config.catalog}`.`{self.config.schema}`.`{self.config.volume}`')
 
         self.spark_query(r'''
             CREATE TABLE IF NOT EXISTS documents (
@@ -62,3 +65,29 @@ class SeaRuntime:
                 created_on          TIMESTAMP
             ) TBLPROPERTIES (delta.enableChangeDataFeed = true)
         ''')
+
+    def ingest_documents(self) -> None:
+        (
+            self.spark
+            .readStream
+            .format('cloudFiles')
+            .option('cloudFiles.format', 'BINARYFILE')
+            .option('pathGlobFilter', '*.pdf')
+            .load(self.config.documents_dir())
+
+            .withColumn('file_hash', F.sha2('content', 256))
+            .withColumn('created_on', F.now())
+            .selectExpr(
+                'path AS file_name',
+                'file_hash',
+                'length AS file_size',
+                'modificationTime AS file_timestamp',
+                'content',
+            )
+
+            .writeStream
+            .trigger(availableNow=True)
+            .option('checkpointLocation', self.config.checkpoints_dir('documents'))
+            .table('documents')
+            .awaitTermination()
+        )
