348b932384d4d5077622222282787ea97e9ee092
Rico A. Beti <rico.beti@silentbyte.com>
2024-05-03 13:04:10 +0800

    Implement placeholder view

--- sea_server/core/views.py
+++ sea_server/core/views.py
@@ -3,4 +3,11 @@
 # # Copyright (c) 2024 SilentByte <https://silentbyte.com/>
 # #
 
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from core import status
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'index.html', status=status.HTTP_503_SERVICE_UNAVAILABLE)
--- sea_server/sea_server/urls.py
+++ sea_server/sea_server/urls.py
@@ -6,6 +6,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from core import views
+
 urlpatterns = [
     path('records/', admin.site.urls),
+    path('', views.index),
 ]
